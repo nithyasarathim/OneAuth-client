84bd0edda4f9e7b731cd50843810d22636989e80
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-12-08 20:12:41 +0530

    setup : react-router-dom setup done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
-    <div className="flex gap-2 w-full h-full justify-center items-bottom my-20">
-      <h1 className="text-sky-500 font-bold text-2xl">ONE</h1><p className="font-bold text-xl mt-0.6">Accounts</p>
+    <div>
+      
     </div>
   )
 }
