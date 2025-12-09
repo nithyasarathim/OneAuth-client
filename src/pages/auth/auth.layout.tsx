7be62ff88541b4f9fc77f82430ef53f4cc62a4e4
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-12-09 22:51:06 +0530

    style: (auth.layout) style refactored for better design

--- src/pages/auth/auth.layout.tsx
+++ src/pages/auth/auth.layout.tsx
@@ -11,13 +11,14 @@ const AuthLayout = ({ children }: AuthLayoutProps) => {
     <div className="min-h-screen w-full flex justify-center items-center px-4 bg-gray-50">
       <motion.div
         layout
-        className="w-full mx-w-md bg-white p-6 border rounded-sm border-gray-300 flex flex-col items-center gap-4"
+        className="w-full max-w-md bg-white p-6 border rounded-sm border-gray-300 flex flex-col items-center gap-4"
       >
         <UserLock className="text-sky-500" size={60} />
         <div className="flex items-center gap-2 text-3xl font-bold">
           <h1 className="text-sky-500">ONE</h1>
           <span>Account</span>
         </div>
+
         <motion.div layout className="w-full min-h-[300px]">
           {children}
         </motion.div>
@@ -25,6 +26,5 @@ const AuthLayout = ({ children }: AuthLayoutProps) => {
     </div>
   );
 };
-
 
 export default AuthLayout;
