011ae1cd4e8c373dce089bdba3e6c3ebdd389612
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-12-09 21:08:53 +0530

    cleanup : (register.page.tsx) page reconstruction for register page

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [tailwindcss(), react()],
+  server: {
+    host: true,
+    port:5173
+  }
 })
