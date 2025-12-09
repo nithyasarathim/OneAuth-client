ae4514f38de18f39703811f13068ecf0a7273948
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-12-09 21:43:31 +0530

    dummy : (register.api.tsx) set with sample request handler for dev env

---
+++ src/pages/auth/register/api/register.api.tsx
@@ -0,0 +1,17 @@
+export const sendEmailVerification = async (email: string) => {
+  await new Promise((r) => setTimeout(r, 1500));
+  console.log(email);
+  return { success: true };
+};
+
+export const verifyOTP = async (otp: string) => {
+  await new Promise((r) => setTimeout(r, 1500));
+  console.log(otp);
+  return { success: true };
+};
+
+export const createAccount = async (email: string, password: string) => {
+  await new Promise((r) => setTimeout(r, 1500));
+  console.log(email, password);
+  return { success: true };
+};
