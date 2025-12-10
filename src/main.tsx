84d5a38062994fed72efc57bc352217b266c55c2
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-12-10 22:53:43 +0530

    feat: react-hot-toast integrated

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,15 @@
-import './assets/index.css';
-import App from './App.tsx'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import "./assets/index.css";
+import App from "./App.tsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster position="top-center" reverseOrder={false} />
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
