e1157c6ae7de97f13803d2e495313ed5f903d2be
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-12-15 11:56:52 +0530

    refactor : (App.tsx) proper route names assigned according to coding standards

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from "react-router-dom";
 
-import RegisterPage from './pages/auth/register/register.page.tsx';
+import RegisterPage from "./pages/auth/register/register.page.tsx";
+import LoginPage from "./pages/auth/login/login.page.tsx";
 
 const App = () => {
   return (
     <Routes>
-      <Route path='/' element={<RegisterPage/>} />
+      <Route path="/auth/register" element={<RegisterPage />} />
+      <Route path="/auth/login" element={<LoginPage />} />
+      <Route path="/" element={
+        <div>
+          I'm home
+        </div>
+      }
+      />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/pages/auth/login/login.page.tsx
+++ src/pages/auth/login/login.page.tsx
@@ -6,6 +6,7 @@ const LoginPage = () => {
       <p className="text-xl font-bold text-center text-gray-900 mb-8 px-6">
         Log in to your Account
       </p>
+      <p>Don't have account ? <span>Create an Account</span></p>
     </AuthLayout>
   );
 };
