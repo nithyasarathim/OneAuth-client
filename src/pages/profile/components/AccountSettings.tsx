2e2ac671f6038bdfbd5384d695402b4873f60c12
Nithyasarathi M <contact.nithyasarathi@gmail.com>
2025-12-18 10:27:24 +0530

    add : (profile) AccountSettings Component added

---
+++ src/pages/profile/components/AccountSettings.tsx
@@ -0,0 +1,9 @@
+const AccountSettings = () => {
+  return (
+    <div className="h-full rounded-2xl bg-emerald-50 flex items-center justify-center text-emerald-700 font-semibold">
+      Account Settings View
+    </div>
+  );
+};
+
+export default AccountSettings;
